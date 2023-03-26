7d3245ff108367925e1655b14a4b6928b9a0aa62
Laurent Bourcin <bourcinlaurent@gmail.com>
2023-03-26 14:21:33 +0200

    html: add header file

---
+++ html/header.php
@@ -0,0 +1,34 @@
+<header class="container-fluid">
+    <!-- Connect buton -->
+    <div class="row">
+        <div class="col-1 offset-11 align-self-end">
+            <button class="btn"> connexion </button>
+        </div>
+    </div>
+    
+    <!-- Logo Le Quai Antique-->
+    <div class="row">
+        <div class="col-12">
+            <img src="../Images/Logo.png" alt="Logo Le Quai Antique">
+        </div>
+    </div>
+
+    <!-- Navbar -->
+    <div class="row">
+        <div class="col-3">
+            <button class="btn"> Accueil </button>
+        </div>
+
+        <div class="col-3">
+            <button class="btn"> Carte </button>
+        </div>
+
+        <div class="col-3">
+            <button class="btn"> Menus </button>
+        </div>
+
+        <div class="col-3">
+            <button class="btn"> Horaires </button>
+        </div>
+    </div>
+</header>
---
+++ html/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Bootstrap/css/bootstrap.css">
+    <title>ECF - Restaurant</title>
+</head>
+<body>
+
+<?php include 'header.php'; ?> 
+
+</body>
+<script src="../Bootstrap/js/bootstrap.bundle.js"></script>
+</html>
