a32f774e9ad0c87164cae729c3546c7ecc19c900
Laurent Bourcin <bourcinlaurent@gmail.com>
2023-03-27 13:56:52 +0200

    css: add display and design for footer.php

--- html/footer.php
+++ html/footer.php
@@ -1,6 +1,6 @@
 <footer class="container-fluid">    
     <div class="row">
-        <div class="col-12">
+        <div class="col-12 bg-dark plt_beige text-center">
             <span>hours<span>
         </div>
     </div>
