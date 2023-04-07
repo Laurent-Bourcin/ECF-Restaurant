abf55e892c695b3335caf53d8a8d5953469b2624
Laurent Bourcin <bourcinlaurent@gmail.com>
2023-04-07 10:51:54 +0200

    admin connection: add admin buttons in food_list.php file and add 3 files with links

--- html/food_list.php
+++ html/food_list.php
@@ -73,6 +73,41 @@ session_start();
     </section>
 </main>
 
+<?php
+    // Admin buttons
+    if (isset($_SESSION['name'])) {
+        if ($_SESSION['type']==='Admin') {
+            ?>
+            <div class="row mt-2 mb-5">
+            <!-- Modify food list -->
+                <div class="col-4 text-center">
+                    <button class="btn bcg_plt_beige plt_golden" 
+                            onclick="window.location.href='./food_modify.php'"> 
+                            Modifier 
+                    </button>
+                </div>
+
+                <!-- Add food list -->
+                <div class="col-4 text-center">
+                    <button class="btn bcg_plt_beige plt_golden" 
+                            onclick="window.location.href='./food_add.php'"> 
+                            Ajouter 
+                    </button>
+                </div>
+
+                <!-- Remove food list -->
+                <div class="col-4 text-center">
+                    <button class="btn bcg_plt_beige plt_golden" 
+                            onclick="window.location.href='./food_remove.php'"> 
+                            Supprimer 
+                    </button>
+                </div>
+            </div>
+        <?php
+        }
+    }
+    ?>
+
 <!-- footer -->
 <?php include 'footer.php'; ?>
 
