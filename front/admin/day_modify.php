37ff0befb27f0f63d5a658283c2cb0879dad3175
Laurent Bourcin <bourcinlaurent@gmail.com>
2023-04-20 14:29:23 +0200

    create days list for admin: add function for modify days into hours.php file

---
+++ front/admin/day_modify.php
@@ -0,0 +1,87 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../Bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="../../css/style.css">
+    <title>ECF_Restaurant_Admin_Jour_Modifier</title>
+</head>
+
+<?php
+// Verification Admin
+if (isset($_SESSION['name'])) {
+    if ($_SESSION['type']==='Admin') {
+        ?>
+
+<main class="container-fluid">
+<div class="row">
+    <div class="col-12 text-center mb-5">
+        <h2> Modifier un jour </h2>
+    </div>
+</div>
+
+<?php 
+// Connection at db
+$mysqli = mysqli_connect("localhost:3307", "root", "", "restaurant");
+?>
+
+<!-- form -->
+<section class="container-fluid mb-5">
+    <form action="day_modify_process.php" method="post">
+        <!-- Type -->
+        <div class="row mb-3 text-center">
+            <div class="col-6 text-end">
+                <label for="day_choice"> Jour à modifier </label>
+            </div>
+            <div class="col-6 text-start">
+                <?php
+                // Choice what to modify
+                $result = $mysqli->query("SELECT title FROM days");
+                ?>
+                <select name="day_choice" id="day_choice">
+                <?php while ($row = mysqli_fetch_array($result)) { ?>
+                    <option value="<?php echo $row['title'] ?>"> 
+                        <?php echo $row['title'] ?> 
+                    </option>
+                <?php }; ?>
+                </select>
+            </div>
+        </div>
+
+        <!-- New entries -->
+        <div class="row mb-3 text-center">
+            <!-- New title -->
+            <div class="col-12 text-center">
+                <label for="new_title"> Nouveau nom: </label>
+                <input type="text" id="new_title" name="new_title">
+            </div>
+        </div>
+
+        <!-- Modify button -->
+        <div class="row mb-3 text-center">
+            <div class="col-12 text-center">
+                <button type="submit" name="modify" value="modify"> Modifier </button>
+            </div>
+        </div>
+    </form>
+</section>
+
+<!-- Back -->
+<div class="col-6">
+<button 
+    class="btn btn-sm bcg_plt_golden px-md-5" 
+    onclick="window.location.href='../hours.php'">
+    <span class="ff_arabic_btn"> Annuler </span>
+</button>
+</div>
+
+<?php
+    }
+};
+?>
