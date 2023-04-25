4eea4af54aa0f7ffee7e8b01ba713204de54e469
Laurent Bourcin <bourcinlaurent@gmail.com>
2023-04-25 11:06:28 +0200

    create hours list: display into footer if restaurant is open/closed with hours

---
+++ Back/footer_display_hours.php
@@ -0,0 +1,42 @@
+<?php
+
+// Connection at db
+$mysqli = mysqli_connect("localhost:3307", "root", "", "restaurant");
+
+date_default_timezone_set('Europe/Paris');
+
+$date_english = date("l");
+$date_french = '';
+// Date in french
+switch($date_english) {
+    case 'Monday':
+        $date_french = 'Lundi';
+        break;
+    case 'Tuesday':
+        $date_french = 'Mardi';
+        break;
+    case 'Wednesday':
+        $date_french = 'Mercredi';
+        break;
+    case 'Thursday':
+        $date_french = 'Jeudi';
+        break;
+    case 'Friday':
+        $date_french = 'Vendredi';
+        break;
+    case 'Saturday':
+        $date_french = 'Samedi';
+        break;
+    case 'Sunday':
+        $date_french = 'Dimanche';
+        break;
+}
+
+// Search days in db hours
+$footer_days_db = $mysqli->query("SELECT day FROM hours");
+$footer_days_exist = [];
+while($footer_days_array = mysqli_fetch_array($footer_days_db)) {
+    if(!in_array($footer_days_array['day'], $footer_days_exist)){
+        array_push($footer_days_exist, $footer_days_array['day']);
+    }
+};
--- front/footer.php
+++ front/footer.php
@@ -1,7 +1,58 @@
+<?php
+require "../Back/footer_display_hours.php";
+?>
+
 <footer class="container-fluid">    
-    <div class="row">
-        <div class="col-12 bg-dark plt_beige text-center">
-            <span>hours<span>
+
+<?php
+/*
+
+            <p> <?php echo $footer_open ?> </p>
+            <p> <?php displayHours() ?> </p>  
         </div>
     </div>
+*/
+?>
+
+<?php
+// If today restaurant is open
+$footer_open = '';
+$footer_title = [];
+$footer_hours = [];
+if(in_array($date_french, $footer_days_exist)) {
+?>
+    <div class="row">
+        <div class="col-12 bg-dark plt_beige text-center">
+            <p> Le restaurant est ouvert aujourd'hui </p>
+        </div>
+    </div>
+
+    <?php
+    // Search hours of day
+        $footer_hours_db = $mysqli->query("SELECT * FROM hours WHERE day='$date_french'");
+        while($footer_hours_array = mysqli_fetch_array($footer_hours_db)) {
+    ?>
+            <!-- If restaurant is open -->
+            <div class="row">
+                <div class="col-12 bg-dark plt_beige text-center">
+                    <p> <?php 
+                    echo $footer_hours_array["title"]. ': '. $footer_hours_array["hours"] 
+                    ?> </p>
+                </div>
+            </div>
+        <?php
+        }
+    } else {
+        ?>
+        <!-- If restaurant is closed -->
+        <div class="row">
+        <div class="col-12 bg-dark plt_beige text-center">
+            <p> Le restaurant est fermé aujourd'hui </p>
+        </div>
+    </div>
+
+    <?php
+    }
+?>
+
 </footer>
